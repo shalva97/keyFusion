33be91ea09b90274cbbb207b51b4cbbaf2f784e2
Nodrex <nchumbadze@gmail.com>
2023-02-13 01:47:45 +0400

    Connection to stable diffusion, decoding response image and saving in to storage

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,4 +39,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.google.code.gson:gson:2.8.8")
 }
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,13 +1,71 @@
 package com.example.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import org.json.simple.JSONObject
+import java.io.ByteArrayInputStream
+import java.io.File
+import java.nio.charset.StandardCharsets
+import java.util.*
+import javax.imageio.ImageIO
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World By CodeCraft!")
+            go()
+        }
+        get("/api/sd") {
+
         }
     }
+}
+
+
+fun go() {
+    val url = "http://127.0.0.1:7860"
+    val client = OkHttpClient()
+    val mediaType = "application/json; charset=utf-8".toMediaType()
+
+    val payload = JSONObject().apply {
+        put("prompt", "Si-Fi space battle car, black background, digital art")
+        put("steps", 20)
+    }
+
+
+    val request = Request.Builder()
+        .url("$url/sdapi/v1/txt2img")
+        .post(RequestBody.create(mediaType, payload.toString()))
+        .build()
+
+    val response = client.newCall(request).execute()
+    val responseBody = response.body!!.string()
+
+
+    val convertedObject = Gson().fromJson(responseBody, JsonObject::class.java)
+
+    val images = convertedObject.getAsJsonArray("images")
+
+    for (i in 0 until images.size()) {
+        val img = images.get(i)
+        var data = img.toString().split(",")[0].replaceFirst("\"", "")
+        data = data.substring(0, data.length - 2)
+
+        val decodedImage = Base64.getDecoder().decode(data.toByteArray(StandardCharsets.UTF_8))
+        val bufferedImage = ImageIO.read(ByteArrayInputStream(decodedImage))
+
+        ImageIO.write(bufferedImage, "png", File("Image_By_CodeCraft_SD.png")) //TODO save in appropriate place
+    }
+
+    println("Done")
+
+
 }
