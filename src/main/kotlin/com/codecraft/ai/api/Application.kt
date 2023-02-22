687f73ee2ffe544c7e7b32ccc787f647b46c18a8
Nodar Tchumbadze <nchumbadze@gmail.com>
2023-02-23 00:23:59 +0400

    mini bug fixed

--- src/main/kotlin/com/codecraft/ai/api/Application.kt
+++ src/main/kotlin/com/codecraft/ai/api/Application.kt
@@ -4,7 +4,6 @@ import com.codecraft.ai.api.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.example.plugins.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
--- src/test/kotlin/com/codecraft/ai/api/ApplicationTest.kt
+++ src/test/kotlin/com/codecraft/ai/api/ApplicationTest.kt
@@ -6,7 +6,6 @@ import io.ktor.client.statement.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 import io.ktor.http.*
-import com.example.plugins.*
 
 class ApplicationTest {
     @Test
